ac3a6f91516e9b6e5fcb9f48dd0e129e5cc30ed2
Dimo Mitev <dimosoftuni@gmail.com>
2023-12-05 21:10:16 +0200

    make the test url configurable

--- tests/ui/footer.test.js
+++ tests/ui/footer.test.js
@@ -1,7 +1,11 @@
 const { test, expect } = require('@playwright/test');
 
+const baseUrl = process.env.URL || 'https://dimosoftuni-boardgames.onrender.com';
+
+console.log("The current URL is: ", baseUrl);
+
 test('Check footer', async ({ page }) => {
-    await page.goto('https://dimosoftuni-boardgames.onrender.com');  
+    await page.goto(`${baseUrl}`);  
     const footer = await page.$('footer');
     const text = await footer.textContent();
     expect(text).toContain('© 2023 - Software Engineering and DevOps regular exam');
