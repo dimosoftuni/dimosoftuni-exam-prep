bea559521bb1cdbe2ab35e9acf846af7d45966b1
Dimo Mitev <dimosoftuni@gmail.com>
2023-12-05 20:11:00 +0200

    Fix tests

--- tests/ui/boardgames.test.js
+++ tests/ui/boardgames.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check boardgames page', async ({ page }) => {
-    await page.goto('http://localhost:8080/collection');
+    await page.goto('http://localhost:8080/boardgames');
     const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
